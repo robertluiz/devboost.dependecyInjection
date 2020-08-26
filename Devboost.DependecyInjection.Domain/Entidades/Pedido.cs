42f6205cf4cf2cb330ca4aa35e78baedfe4edb8b
Guilherme Cupertino <guilherme_cupertino@hotmail.com>
2020-08-26 20:40:17 -0300

    repositorios

--- Devboost.DependecyInjection.Domain/Entidades/Pedido.cs
+++ Devboost.DependecyInjection.Domain/Entidades/Pedido.cs
@@ -3,7 +3,6 @@
     public class Pedido : EntidadeBase<int>
     {
         public string Descricao { get; set; }
-        public Cliente Cliente { get; set; }
-        
+        public Cliente Cliente { get; set; }        
     }
 }
---
+++ Devboost.DependecyInjection.Domain/Interface/DomainService/IPedidoDomainService.cs
@@ -0,0 +1,13 @@
+﻿using Devboost.DependecyInjection.Domain.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Devboost.DependecyInjection.Domain.Interface.DomainService
+{
+    public interface IPedidoDomainService
+    {
+        Task<List<Pedido>> Listar();
+    }
+}
