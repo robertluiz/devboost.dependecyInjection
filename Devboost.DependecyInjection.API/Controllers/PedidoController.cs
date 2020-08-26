45b7dd5e28d15ac4c6e14ea9f938deb4887209d7
Robert Luiz <robert.tiweb@gmail.com>
2020-08-26 20:37:31 -0300

    wip controller

---
+++ Devboost.DependecyInjection.API/Controllers/PedidoController.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Devboost.DependecyInjection.API.Controllers
+{
+    public class PedidoController : Controller
+    {
+        // GET
+        public async Task<IActionResult> Listar()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ Devboost.DependecyInjection.API/Controllers/ProdutoController.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Devboost.DependecyInjection.API.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        // GET
+        public async Task<IActionResult> Listar()
+        {
+            return Ok();
+        }
+    }
+}
