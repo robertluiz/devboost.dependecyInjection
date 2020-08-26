1148594cf283d22ac92d77cdcc80ffe354b25966
italovinicios <italovinicios@gmail.com>
2020-08-26 20:32:30 -0300

    interface listarProdutos

---
+++ Devboost.DependecyInjection.Domain/Interface/DomainService/IDomainServiceListarProdutos.cs
@@ -0,0 +1,12 @@
+﻿using Devboost.DependecyInjection.Domain.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Devboost.DependecyInjection.Domain.Interface.DomainService
+{
+    public interface IDomainServiceListarProdutos
+    {
+         List<Produto> Executar();
+    }
+}
