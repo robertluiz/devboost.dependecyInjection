550a7fa9d29bb3b4dcf1d1365fc11f4a6171d385
Robert Luiz <robert.tiweb@gmail.com>
2020-08-26 20:55:19 -0300

    controllers

--- Devboost.DependecyInjection.API/Controllers/PedidoController.cs
+++ Devboost.DependecyInjection.API/Controllers/PedidoController.cs
@@ -1,14 +1,23 @@
 ﻿using System.Threading.Tasks;
+using Devboost.DependecyInjection.Domain.Interface.DomainService;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Devboost.DependecyInjection.API.Controllers
 {
     public class PedidoController : Controller
     {
+        private readonly IPedidoDomainService _pedidoService;
+
+        public PedidoController(IPedidoDomainService pedidoService)
+        {
+            _pedidoService = pedidoService;
+        }
+
         // GET
         public async Task<IActionResult> Listar()
         {
-            return Ok();
+            var pedidos = await _pedidoService.Listar();
+            return Ok(pedidos);
         }
     }
 }
--- Devboost.DependecyInjection.API/Controllers/ProdutoController.cs
+++ Devboost.DependecyInjection.API/Controllers/ProdutoController.cs
@@ -1,14 +1,23 @@
 ﻿using System.Threading.Tasks;
+using Devboost.DependecyInjection.Domain.Interface.DomainService;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Devboost.DependecyInjection.API.Controllers
 {
     public class ProdutoController : Controller
     {
+        private readonly IProdutoDomainService _produtoService;
+
+        public ProdutoController(IProdutoDomainService produtoService)
+        {
+            _produtoService = produtoService;
+        }
+
         // GET
         public async Task<IActionResult> Listar()
         {
-            return Ok();
+            var produtos = await _produtoService.Listar();
+            return Ok(produtos);
         }
     }
 }
