f3dceb66c1e59bafca5b5fefeadcec24db23501d
Ramsés França <ramsesfranca@hotmail.com>
2020-08-26 20:17:37 -0300

    Entidade Base

---
+++ Devboost.DependecyInjection.Domain/Entidades/EntidadeBase.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Devboost.DependecyInjection.Domain.Entidades
+{
+    public abstract class EntidadeBase<TId>
+        where TId : IEquatable<TId>
+    {
+        public TId Id { get; set; }
+    }
+}
